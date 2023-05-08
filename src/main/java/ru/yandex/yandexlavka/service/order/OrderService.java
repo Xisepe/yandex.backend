8060e2cd36632f26f742fa5d3640005dd7a506ef
Maxim <maxim.golubeff2014@gmail.com>
2023-05-09 00:47:40 +0700

    OrderService interface

---
+++ src/main/java/ru/yandex/yandexlavka/service/order/OrderService.java
@@ -0,0 +1,16 @@
+package ru.yandex.yandexlavka.service.order;
+
+import ru.yandex.yandexlavka.dto.order.CreateOrderRequest;
+import ru.yandex.yandexlavka.dto.order.OrderDto;
+import ru.yandex.yandexlavka.request.CompleteOrderRequestDto;
+import ru.yandex.yandexlavka.response.OrderAssignResponse;
+
+import java.util.List;
+
+public interface OrderService {
+    OrderDto createOrder(CreateOrderRequest createOrderRequest);
+    List<OrderDto> getOrdersWithLimitAndOffset(int limit, int offset);
+    OrderDto getOrderByIdOrThrow(long orderId);
+    List<OrderDto> completeOrders(CompleteOrderRequestDto completeOrderRequestDto);
+    List<OrderAssignResponse> assignOrdersByDate(String date);
+}
