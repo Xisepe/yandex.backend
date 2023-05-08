90be31f19465468d2fe796048ce40e9db60a5524
Maxim <maxim.golubeff2014@gmail.com>
2023-05-09 01:36:01 +0700

    OrderService

--- src/main/java/ru/yandex/yandexlavka/service/order/OrderServiceImpl.java
+++ src/main/java/ru/yandex/yandexlavka/service/order/OrderServiceImpl.java
@@ -3,8 +3,12 @@ package ru.yandex.yandexlavka.service.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.yandexlavka.domain.order.Order;
+import ru.yandex.yandexlavka.dto.order.CompleteOrder;
 import ru.yandex.yandexlavka.dto.order.CreateOrderRequest;
 import ru.yandex.yandexlavka.dto.order.OrderDto;
+import ru.yandex.yandexlavka.exceptions.general.InvalidRequestException;
+import ru.yandex.yandexlavka.exceptions.order.InvalidCompleteOrderRequestDtoException;
+import ru.yandex.yandexlavka.exceptions.order.OrderNotFoundException;
 import ru.yandex.yandexlavka.repository.order.OrderRepository;
 import ru.yandex.yandexlavka.request.CompleteOrderRequestDto;
 import ru.yandex.yandexlavka.response.OrderAssignResponse;
@@ -12,6 +16,7 @@ import ru.yandex.yandexlavka.service.mapper.order.CreateOrderDtoToOrderMapper;
 import ru.yandex.yandexlavka.service.mapper.order.OrderToOrderDtoMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -34,16 +39,30 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderDto> getOrdersWithLimitAndOffset(int limit, int offset) {
-        return null;
+        return orderRepository.findAll().stream()
+                .skip(offset)
+                .limit(limit)
+                .map(orderToOrderDtoMapper::mapToDto)
+                .toList();
     }
 
     @Override
     public OrderDto getOrderByIdOrThrow(long orderId) {
-        return null;
+        Optional<Order> order = orderRepository.findById(orderId);
+        if (order.isEmpty()) {
+            throw new OrderNotFoundException();
+        }
+        return orderToOrderDtoMapper.mapToDto(order.get());
     }
 
     @Override
     public List<OrderDto> completeOrders(CompleteOrderRequestDto completeOrderRequestDto) {
+        for (CompleteOrder completeOrder : completeOrderRequestDto.getCompleteInfo()) {
+            Optional<Order> order = orderRepository.findById(completeOrder.getOrderId());
+            if (order.isEmpty()) {
+                throw new InvalidCompleteOrderRequestDtoException();
+            }
+        }
         return null;
     }
 
