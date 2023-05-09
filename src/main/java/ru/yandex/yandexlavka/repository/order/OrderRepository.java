f13328beb5c6906259c3089a97b3e87cb1e9f033
Maxim <maxim.golubeff2014@gmail.com>
2023-05-10 00:22:10 +0700

    started task2

--- src/main/java/ru/yandex/yandexlavka/repository/order/OrderRepository.java
+++ src/main/java/ru/yandex/yandexlavka/repository/order/OrderRepository.java
@@ -2,8 +2,14 @@ package ru.yandex.yandexlavka.repository.order;
 
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.yandex.yandexlavka.domain.courier.Courier;
 import ru.yandex.yandexlavka.domain.order.Order;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends ListCrudRepository<Order, Long> {
+    List<Order> findAllByCourierIdAndCompleteTimeBetween(long courierId, LocalDateTime startDate, LocalDateTime endDate);
 }
